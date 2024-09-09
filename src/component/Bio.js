ffbafa07b35d6e0f2d96e14235e25a56c9c52f45
Chaser-254 <81488755+Chaser-254@users.noreply.github.com>
2024-09-09 09:01:31 +0300

    modifications for Bio.Bio.js

--- src/component/Bio.js
+++ src/component/Bio.js
@@ -9,7 +9,7 @@ const Bio = () => {
                 <b className="hello">Hello</b> <br/>
                 I am <b className="my-name">Emmanuel Erogo Esibitar</b>, a{" "}
                 <b className="my-name">Sofwtare Engineer</b> with a 3+ years of
-                experince in designing,implementing and testing  software applications. My expertise lies in developing scalable backend and high perfomance applications using modern framework such like Django and Node.js as well as 
+                experience in designing,implementing and testing  software applications. My expertise lies in developing scalable backend and high perfomance applications using modern framework such like Django and Node.js as well as 
                 frontend technologies like React. I am committed to creating innovative solutions 
                 that meets or exceed client expectations and to deliver a measurable business solution.
             </div>
